adb74d926a08d9621b30c3622f118a6fda442f91
Harsh Kansara <harshkansara98@gmail.com>
2021-06-21 18:06:46 +1200

    Added the navbar section.

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
+import { Navbar } from "../../components/navbar";
+
 const PageContainer = styled.div`
     ${tw`
         flex
@@ -14,7 +16,9 @@ const PageContainer = styled.div`
 `;
 
 export function HomePage() {
-    return <PageContainer>
-        Hello World!
-    </PageContainer>
+    return (
+        <PageContainer>
+            <Navbar/>
+        </PageContainer>
+    );    
 }
